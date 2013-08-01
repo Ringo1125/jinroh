8ae7dfc1cb1c1dc50a49b4cd9634f7100010e305
Ringo1125 <1125yukino+dev@gmail.com>
2013-08-01 21:08:04 +0900

    added main.css,modified index.html,main.js
    index.html:added references to the libraries.
    main.js:added classes:Player,GameRoom.

--- jinroh/jinroh/main.js
+++ jinroh/jinroh/main.js
@@ -1,3 +1,36 @@
 /**jinroh/main.js
- * 2013/08/01 by Yukino
+ * 2013/08/01 by Ringo1125
  */
+/**
+ * @class Player describes player of this game.
+ * @Constructor
+ * @param {String}name
+ *            name of player.
+ * @param {number}role
+ *            role of player.
+ * @Extends Backbone.model
+ */
+var Player = Backbone.Model.extend({// instance property
+	initialize : function(name, role) {
+
+	},
+	name : "",
+	role : 0
+}, {// class property
+	ROLE_VILLAGER : 0,
+	ROLE_WEREWOLF : 1,
+	ROLE_PSYCHIC : 2,
+	ROLE_FORTUNETELLER : 3,
+	ROLE_HUNTER : 4,
+	ROLE_INSANE : 5,
+});
+/**
+ * @Class GameRoom
+ * 
+ * 
+ */
+var GameRoom = Backbone.Collection.extend({// instance property
+
+}, {// class property
+
+});
