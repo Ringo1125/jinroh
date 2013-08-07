db6bbf6d78c925ffb2229b84d04593bd684f3ff8
Ringo1125 <1125yukino+dev@gmail.com>
2013-08-08 00:03:50 +0900

    表示、非表示について考え中。

--- jinroh/jinroh/test.js
+++ jinroh/jinroh/test.js
@@ -1,7 +1,7 @@
 /**
  * jinroh/test.js 2013/08/07 by Yukino
  */
-$('#role-custom').click(function() {
-	// クリックイベントで要素をトグルさせる
-	$("#config-number-option").slideToggle(this.checked);
-});
+// $('#role-custom').click(function() {
+// // クリックイベントで要素をトグルさせる
+// $("#config-number-option").slideToggle(this.checked);
+// });
