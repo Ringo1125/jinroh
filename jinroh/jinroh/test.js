ec340a5e6d8c0e123a6555fc9bf9e7f70f9de8c0
Ringo1125 <1125yukino+dev@gmail.com>
2013-08-10 21:36:30 +0900

    人数設定の見た目が納得できるものになりました！

--- jinroh/jinroh/test.js
+++ jinroh/jinroh/test.js
@@ -1,3 +1,18 @@
 /**
  * jinroh/test.js 2013/08/07 by Yukino
  */
+$(document).ready(function($) {
+	alert("get ready");
+	$("#role-custom").click(function() {
+		// alert("checked");
+		if (this.checked) {
+			$("#config-number-option").show("nornal", function() {
+				// alert("showing!");
+			});
+		} else {
+			$("#config-number-option").hide("nornal", function() {
+				// alert("see you!");
+			});
+		}
+	});
+});
